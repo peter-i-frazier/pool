7c15dc038e1fc696f649789865b21a51b9b48e85
Jialei Wang <jialeiwang@jw865-2.orie.cornell.edu>
2013-11-19 23:41:07 -0500

    prob shortest hit

---
+++ src/compare_maxP_vs_naive/prob_shortest_hit.R
@@ -0,0 +1,41 @@
+prob_shortest_hit <- function(peptides, prob, b) {
+#================================================================================
+# Calculate P(shortest hit <= b)
+	idx <- c()
+	no <- 0
+	if (is.vector(peptides)) {
+		if (get_length(peptides)<=b){
+			result <- prob
+		} else {
+			result <- 0
+		}
+	} else {
+		for (n in 1:dim(peptides)[1]) {
+			if (get_length(peptides[n,])<=b) {
+				idx <- c(idx, n)
+				no <- no +1
+			}
+		}
+		if (no == 0) {
+			result <- 0
+		} else {
+			prod <- 1
+			for (i in 1:length(idx)) {
+				prod <- prod * (1.0-prob(idx[i]))
+			}
+			result <- 1.0-prod
+		}
+	}
+	return (result)
+}
+
+get_length <- function(peptide) {
+	len <- 1
+	for (i in 1:length(peptide)) {
+		if (peptide[i] != -1){
+			len <- len + 1
+		}
+	}
+	return (len)
+}
+
