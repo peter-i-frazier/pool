be3633835684ab386ac3d1b69ee4bd32637a72ef
Peter Frazier <peter.i.frazier@gmail.com>
2013-12-06 15:12:29 -0500

    Adding some descriptive information to matlabPlot.m so that I know what it plots, and how to recreate it.

--- src/compare_maxP_vs_naive/matlabPlot.m
+++ src/compare_maxP_vs_naive/matlabPlot.m
@@ -1,4 +1,13 @@
-a=csvread('PP_use_newData#2.csv',1,1);
+% This datafile was originally called PP_use_newData#2.csv
+% It contains P(shortest hist <= 12) vs. no. of recommendations for three
+% different methods,
+% maxP is choosing the next thing to add to maximize probability of
+% improvement, adding 10 at a time;
+% ranking estimated probability;
+% mutation, averaged over 100 independent replications, where in each
+% replication S contains the first |S| of a set of 100 randomly mutated
+% peptides.
+a=csvread('forplot.csv',1,1); 
 h=figure;
 hold;
 plot(1:100,a(1,:),'r');
