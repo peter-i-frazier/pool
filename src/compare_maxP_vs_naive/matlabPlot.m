d920fa2ddf368f89b13ede1f459faf31f64ac2ed
Jialei Wang <jialeiwang@jw865-2.orie.cornell.edu>
2013-12-23 11:04:39 -0500

    writeup updated

--- src/compare_maxP_vs_naive/matlabPlot.m
+++ src/compare_maxP_vs_naive/matlabPlot.m
@@ -1,4 +1,4 @@
-a=csvread('PP_use_newData#2.csv',1,1);
+a=csvread('forplot.csv',1,1);
 h=figure;
 hold;
 plot(1:100,a(1,:),'r');
@@ -7,5 +7,5 @@ plot(1:100,a(3,:),'b')
 xlabel('No. of recommendations','FontSize',30);
 ylabel('P(shortest hit <= 12)','FontSize',30);
 axis([0 100 0 1.2]);
-legend('maxP','Ranking estimated probability of hit','mutate');
-print(h,'-dpng','maxP_comparison.png');
+legend('Probability of improvement algorithm','Pick most probable from posterior','Mutate from known hits');
+print(h,'-dpdf','maxP_comparison.pdf');
