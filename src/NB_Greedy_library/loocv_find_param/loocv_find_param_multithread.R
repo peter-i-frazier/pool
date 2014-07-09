43279cad564324dbe80429d55e4faaa9efd3ee98
JiaLei Wang <jw865@whale.icse.cornell.edu>
2014-07-09 02:15:31 -0400

    loocv find param parallel

--- src/NB_Greedy_library/loocv_find_param/loocv_find_param_multithread.R
+++ src/NB_Greedy_library/loocv_find_param/loocv_find_param_multithread.R
@@ -1,29 +1,26 @@
 rm(list=ls())
 #Parallel Computation Packages
-require(multicore)
 require(snow)
 require(doParallel)
-require(MCMCpack)
-require(Rlab)
 #=================================================================================
 #Parallel Computation Setting
 # hosts <- rep('whale',40)
 # hosts <- c(rep("whale",52),rep("ahab",24),rep("flask",24),rep("starbuck",24))
-hosts <- c(rep("whale",30),rep("ahab",30),rep("flask",30),rep("starbuck",30))
-# hosts <- c(rep("ishmael",30),rep("stubb",30),rep("daggoo",30),rep("tashtego",30))
+# hosts <- c(rep("whale",30),rep("ahab",30),rep("flask",30),rep("starbuck",30))
+hosts <- c(rep("ishmael",30),rep("stubb",30),rep("daggoo",30),rep("tashtego",30))
 cl <- makeCluster(hosts, type = "SOCK")
 registerDoParallel(cl)
 
 root_path = "/fs/home/jw865/peptide-catalysis"
 load(paste(root_path, "/data/all_data_for_training.RData", sep=''))
 
-outcome_name <- "sfp"
-X <- trainX_sfp
-Y <- trainY_sfp
+outcome_name <- "AcpS"
+X <- trainX_AcpS
+Y <- trainY_AcpS
 
-gamma_0_list <- c(1000, 500, 100, 50, 10, 1)
+gamma_0_list <- c(500, 100, 50, 10, 1)
 gamma_1_list <- c(0.01, 0.05, 0.1, 0.5, 1)
-prior_list <- c(1e-5, 1e-4, 1e-3, 1e-2, 0.1, 0.3, 0.5)
+prior_list <- c(1e-5, 1e-4, 1e-3, 1e-2, 0.1, 0.5)
 
 nL <- 19
 nR <- 19
@@ -50,7 +47,7 @@ for (i in 1:length(gamma_0_list)) {
             print (count)
             print (outcome_name)
             index[count,] <- c(i,j,k)
-            prob <- foreach (n = 1:dim(X)[1], .init = c(), .combine = c, .packages = 'Rlab', .inorder = T, .errorhandling = 'pass') %dopar% {
+            prob <- foreach (n = 1:dim(X)[1], .init = c(), .combine = c, .inorder = T, .errorhandling = 'pass') %dopar% {
                     Naive_Bayes(X[-n,], Y[-n], X[n,], AAclass, S.Pos, maxL, maxR, gamma_0_list[i], gamma_1_list[j], prior_list[k], predIter)
             }
             xy <- ROC_xy_output(prob, Y)
