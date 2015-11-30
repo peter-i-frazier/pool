ecb581284df3f407312fcc8b67138f95f326f4d6
Tom Fei <yf275@stubb.icse.cornell.edu>
2015-11-30 17:44:56 -0500

    Add some comments
    Former-commit-id: acf82ddcdb7c0d8ffd552e03d0bd9892e46ca508

--- src/review15/visualization.py
+++ src/review15/visualization.py
@@ -10,7 +10,7 @@ Functions computing and visualizing distance between peptides
 # Penalties, must have length of 3 and that
 # pens[0] > pens[1] > pens[2] > 0
 # Modify to make plot perfect
-pens = [3., 2., 1.]
+pens = [20., 2., 1.]
 # DO NOT modify if you do not know what you are doing
 class_dict = {'A': 5,
               'R': 4,
@@ -72,15 +72,15 @@ def compute_dist_one_side(str1, str2):
 #    return pen1 + pen2
 
 def compute_dist(strlist1, strlist2):
-    print '******Computing dist*******'
+    #print '******Computing dist*******'
     # Substrings before Serine must be reversed
     # before being parsed to function
     pen1 = compute_dist_one_side(strlist1[0][::-1],
                                  strlist2[0][::-1])
-    print 'left pen', pen1
+    #print 'left pen', pen1
     pen2 = compute_dist_one_side(strlist1[1],
                                  strlist2[1])
-    print 'right pen', pen2
+    #print 'right pen', pen2
     return pen1 + pen2
 
 def compute_dist_mat(X):
@@ -90,6 +90,7 @@ def compute_dist_mat(X):
     return: numpy.ndarray, distance between strings
             a square and symmetric matrix with dim len(X)
     """
+    print 'Computing distance matrix...'
     mat = np.zeros((len(X), len(X)))
     for i in xrange(len(mat)):
         for j in xrange(i):
@@ -97,12 +98,15 @@ def compute_dist_mat(X):
             d = compute_dist(X[i, :], X[j, :])
             mat[i, j] = d
             mat[j, i] = d
+    print 'Done'
     return mat
 
 def compute_rel_pos(X):
     dist_mat = compute_dist_mat(X)
+    print 'Computing coordinates for plotting...'
     tsne = TSNE(metric='precomputed', random_state=0)
     rel_pos = tsne.fit_transform(dist_mat)
+    print 'Done'
     np.savetxt('rel_pos.csv', rel_pos, delimiter=',')
     return rel_pos
 
@@ -112,24 +116,31 @@ def plot(X, n_orig):
     rel_pos = compute_rel_pos(X)
     N = (len(X) - n_orig) / 3  # size of other groups
     colors = np.concatenate((np.zeros(n_orig), np.repeat([1, 2, 3], N)))
+    plt.figure()
     plt.scatter(rel_pos[:, 0], rel_pos[:, 1],
                 s=40, c=colors, alpha=.7, edgecolors='none')
+    plt.title('alpha={0}, beta={1}, gamma={2}'.format(pens[0], pens[1], pens[2]))
     plt.savefig('plot.pdf')
 
-def clean_data(X):
-    orig = X[:, 0]
+## No need to call this when data is generated from get_data.py
+def format_data(X):
     # Compute number of original peptides
+    orig = X[:, 0]
     n_orig = len(orig[orig != ''])
+    # Reformat data into two columns
     X_clean = np.vstack(np.split(X, [2, 4, 6], axis=1))
     return X_clean[~np.any(X_clean == '', axis=1)], n_orig
 
 if __name__ == '__main__':
+    n_orig = 835
     # numpy array of numpy strings
     #X = np.genfromtxt('test_input.csv', delimiter=',', dtype=str)
-    X = np.genfromtxt('toy_visualization_data.csv',
-                      delimiter=',', dtype=str, skip_header=1)
-    X, n_orig = clean_data(X)
+    X = np.genfromtxt('data.csv', delimiter=',', dtype=str)
+    #X = np.genfromtxt('toy_visualization_data.csv',
+    #                  delimiter=',', dtype=str, skip_header=1)
+    #X, n_orig = format_data(X)
     #print X
     #print compute_dist_mat(X)
     plot(X, n_orig)
+    print 'Penalties', pens
     print 'n_orig =', n_orig
