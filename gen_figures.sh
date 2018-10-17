9ac0b1a0fde73b9896066f5d666c31d7bbd77259
Peter Frazier <pf98@cornell.edu>
2018-10-17 10:24:24 -0400

    using python3 instead of python in gen_figures.sh

--- gen_figures.sh
+++ gen_figures.sh
@@ -4,7 +4,7 @@ Rscript gen_roc_data.R
 Rscript gen_benchmark.R 100 sfp AcpS sfp_specific
 Rscript gen_benchmark.R 100 AcpS sfp acps_specific
 
-python gen_simulation_coord.py sfp
-python gen_simulation_coord.py acps
+python3 gen_simulation_coord.py sfp
+python3 gen_simulation_coord.py acps
 
-python gen_figures.py
+python3 gen_figures.py
